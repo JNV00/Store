31327ea7ee6daf8e80e809855169ceeecfaba2e7
ParroquiaSantaVeracruz <parroquiasantaveracruz2@gmail.com>
2021-06-21 13:39:53 -0500

    Lunes

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Vector;
+
+
+/**
+ *
+ * @author Yo
+ */
+public class Usuario {
+    private String id;
+    private String nombreCuenta;
+    private int edad;
+    private String correo;
+    private String contraseña;
+    private Vector listaDeseos;
+    
+    
+}
